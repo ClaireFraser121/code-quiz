9e4096fb7589ad74ffc38b21d1b9e6a1791d3fb1
Claire Fraser <clairefraser121@gmail.com>
2023-11-04 13:42:29 +0000

    creating pseudocode

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,4 @@
+// Create var for 5 questions with 3 or 4 opitons for the answers
+// create an array of objects for each questions and answer
+// make an array with the answers 
+// then create an if statement 
