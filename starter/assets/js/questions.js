52f88a8bb7439f093283d92f3a63f90878716596
Claire Fraser <clairefraser121@gmail.com>
2023-11-06 12:52:54 +0000

    Creating the pseudocode for the js questions

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,4 +1,22 @@
-// Create var for 5 questions with 3 or 4 opitons for the answers
-// create an array of objects for each questions and answer
-// make an array with the answers 
-// then create an if statement 
+// Create an Array for Questions:
+
+// Start by creating an array to store all the questions for your quiz. Each element in the array represents a question.
+
+// Each question can be an object containing properties like questionText, answerChoices, and correctAnswer. For example:
+
+
+const questions = [
+  {
+    questionText: "What does HTML stand for?",
+    answerChoices: ["Hyper Text Markup Language", "Hyperlink and Text Markup Language", "High Text Markup Language"],
+    correctAnswer: 0 // Index of the correct answer in the answerChoices array
+  },
+  // Add more question objects as needed
+];
+
+// Question Object Properties:
+
+// Each question object should have the following properties:
+// questionText: A string that contains the text of the question.
+// answerChoices: An array of strings that represents the answer choices for the question.
+// correctAnswer: An integer representing the index of the correct answer within the answerChoices array.
